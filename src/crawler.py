73355f056f574bb1597b1ffd09a3cc16bb926e74
HoneyJung <37574790+HoneyJung@users.noreply.github.com>
2019-09-10 17:42:35 -0700

    dir arrange

--- src/crawler.py
+++ src/crawler.py
@@ -3,11 +3,30 @@ from bs4 import BeautifulSoup
 import urllib.request
 import urllib.parse
 import logging
+import time
 
 driver = webdriver.Chrome('./chromedriver')
 url = "http://sogang.ac.kr/bachelor/students/notice/notice08.html"
-xpath = "//*[@id='mainFrm']"
-
 driver.get(url)
-element = driver.find_element_by_xpath(xpath)
-print(element.get_attribute('outerHTML'))
+time.sleep(10)
+xpath = "/html/body/div/div"
+element = driver.find_elements_by_css_selector("body > div > div")
+#element = driver.find_element_by_xpath(xpath)
+print(element.text)
+
+
+'''
+all_children_by_xpath = element.find_elements_by_xpath(".//*")
+for child in all_children_by_xpath:
+    print("new")
+    print(type(child))
+    print(child.text)
+
+'''
+#print("children : " + str(all_children_by_xpath))
+
+
+
+
+# print("html : " + str(element.get_attribute('innerHTML')))
+
